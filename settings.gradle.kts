67e5799471b1f5a5473eff5a9c3c59f3654f5ad2
vicky leu <yu6564172@gmail.com>
2024-10-06 06:14:42 +0800

    ios local file controller should close

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -116,15 +116,7 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
 
     repositories {
-        mavenCentral{
-            content {
-                includeGroupByRegex("io.github.*")
-                excludeGroupByRegex("org.jogamp.*")
-                excludeGroupByRegex("com.vickyleu.*")
-                excludeGroupByRegex("org.jetbrains.compose.*")
-                excludeGroupByRegex("com.github.(?!johnrengelman|oshi|bumptech|mzule|pwittchen|filippudak|asyl|florent37).*")
-            }
-        }
+        mavenCentral()
         google {
             content {
                 excludeGroupByRegex("org.jogamp.*")
@@ -136,66 +128,7 @@ dependencyResolutionManagement {
             }
         }
 
-        // workaround for https://youtrack.jetbrains.com/issue/KT-51379
-        maven { setUrl("https://repo.maven.apache.org/maven2")
-            content {
-                excludeGroupByRegex("org.jogamp.*")
-                excludeGroupByRegex("org.jetbrains.compose.*")
-                excludeGroupByRegex("com.vickyleu.*")
-                excludeGroupByRegex("com.github.(?!johnrengelman|oshi|bumptech|mzule|pwittchen|filippudak|asyl|florent37).*")
-            }
-        }
-        ivy {
-            name = "Node.js"
-            setUrl("https://nodejs.org/dist")
-            patternLayout {
-                artifact("v[revision]/[artifact](-v[revision]-[classifier]).[ext]")
-            }
-            metadataSources {
-                artifact()
-            }
-            content {
-                excludeGroupByRegex("org.jogamp.*")
-                includeModule("org.nodejs", "node")
-            }
-            isAllowInsecureProtocol = false
-        }
-        maven {
-            setUrl("https://jitpack.io")
-            content {
-                excludeGroupByRegex("org.jogamp.*")
-                includeGroupByRegex("com.github.*")
-                includeGroupByRegex("io.github.*")
-                excludeGroupByRegex("org.jetbrains.compose.*")
-            }
-        }
-        maven {
-            setUrl("https://repo1.maven.org/maven2/")
-            content {
-                excludeGroupByRegex("org.jetbrains.compose.*")
-                includeGroupByRegex("org.jogamp.gluegen.*")
-            }
-        }
-        maven {
-            setUrl("https://maven.aliyun.com/repository/public/")
-            content {
-                excludeGroupByRegex("org.jogamp.*")
-                includeGroupByRegex("com.aliyun.*")
-                excludeGroupByRegex("com.vickyleu.*")
-                excludeGroupByRegex("org.jetbrains.compose.*")
-                excludeGroupByRegex("com.github.vickyleu.*")
-                excludeGroupByRegex("io.github.vickyleu.*")
-            }
-        }
-        maven {
-            setUrl("https://maven.aliyun.com/nexus/content/repositories/jcenter/")
-            content {
-                excludeGroupByRegex("org.jogamp.*")
-                excludeGroupByRegex("com.vickyleu.*")
-                excludeGroupByRegex("org.jetbrains.compose.*")
-                excludeGroupByRegex("com.github.(?!johnrengelman|oshi|bumptech|mzule|pwittchen|filippudak|asyl|florent37).*")
-            }
-        }
+
         maven { setUrl("https://maven.pkg.jetbrains.space/public/p/compose/dev")
             content {
                 excludeGroupByRegex("org.jogamp.*")
@@ -220,13 +153,7 @@ dependencyResolutionManagement {
                 excludeGroupByRegex("io.github.*")
             }
         }
-        maven {
-            setUrl("https://jogamp.org/deployment/maven")
-            content {
-                includeGroupByRegex("dev.datlag.*")
-                excludeGroupByRegex("org.jetbrains.compose.*")
-            }
-        }
+
         maven("https://maven.pkg.jetbrains.space/kotlin/p/wasm/experimental"){
             content {
                 excludeGroupByRegex("org.jogamp.*")
